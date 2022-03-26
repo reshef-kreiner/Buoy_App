ae69d6530652b6a947e8504c33030408cbca2b61
reshef-kreiner <reshef31@gmail.com>
2022-03-26 19:37:56 +0300

    after adding the splash screen - need to make header disapear

---
+++ app/src/main/java/com/example/buay_app/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.buay_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    private Handler mWaitHandler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
